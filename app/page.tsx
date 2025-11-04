c35d7a8993674ea1e2d10a6f2cbde79f81d7f1f4
Nicholas Wise <nicholasaragih@gmail.com>
2025-11-04 16:23:17 +0700

    feat: change abit of styling

--- app/page.tsx
+++ app/page.tsx
@@ -14,88 +14,44 @@ import RightPillar from "@/public/right-pillar.webp";
 import Star from "@/public/starss.webp";
 import WhatIs from "@/public/what-is.svg";
 import WhatIsBox from "@/public/what-is-box.svg";
-import VisiMisi from "@/public/visi-misi.svg";
+import VisiMisi from "@/public/visi-misi.webp";
 import PTDTitle from "@/public/PTDTitle.svg"
 import Clock from "@/public/clock.svg"
 import Story from "@/public/story.svg"
+import HeroBG from "@/public/herobg.svg"
 import LeftPattern from "@/public/leftpattern.svg"
 import RightPattern from "@/public/rightpattern.svg"
 
-const page = () => {
+const page = () => { 
   return (
     <main>
       <div className="overflow-hidden bg-gradient-to-b from-black via-[#44340C] to-[#271E08] w-full min-h-screen flex flex-col relative items-center justify-center">
         <Image src={Star} alt="Bg" className="absolute w-full opacity-40" />
-        <Image
-          src={Light}
-          alt="Logo"
-          className="absolute w-[500px] -top-[3%] -left-[7%]"
-        />
-        <Image
-          src={Light}
-          alt="Logo"
-          className="absolute w-[500px] -top-[3%] -right-[7%] -scale-x-100"
-        />
+        <motion.div
+          initial={{ opacity: 0, y: -80 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.3, ease: "easeOut" }}
+          className="absolute top-0 left-[44%] w-[200px] mx-auto mt-10 opacity-90"
+        >
         <Image
           src={Sun}
           alt="Logo"
-          className="absolute top-0 left-[44%] w-[200px] mx-auto mt-10 opacity-90"
         />
-
-        <motion.div
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, delay: 0.3, ease: "easeOut" }}
-          className="absolute bottom-0 w-full"
-        >
-          <Image src={Landscape} alt="Landscape" className="w-full" />
         </motion.div>
 
         <motion.div
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, delay: 0.3, ease: "easeOut" }}
-          className="absolute -bottom-10 md:bottom-5 lg:bottom-12 left-10 lg:left-20 w-[250px] md:w-[300px] lg:w-[400px] xl:w-[500px]"
+          className="absolute bottom-0 w-full "
         >
-          <Image src={Building} alt="Building" className="w-full h-auto" />
+          <Image src={HeroBG} alt="Landscape" className="w-full" />
         </motion.div>
 
-        <motion.div
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, delay: 0.3, ease: "easeOut" }}
-          className="absolute bottom-16 md:bottom-24 lg:bottom-30 xl:bottom-36 right-10 w-[200px] md:w-[400px] lg:w-[500px]"
-        >
-          <Image src={Tree} alt="Tree" className="w-full h-auto" />
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0, x: -200 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1, ease: "easeOut" }}
-          className="absolute hidden lg:block bottom-10 -left-40 w-[350px]" // container gets positioning
-        >
-          <Image
-            src={LeftPillar}
-            alt="Left Pillar"
-            className="w-full h-auto"
-          />
-        </motion.div>
 
-        <motion.div
-          initial={{ opacity: 0, x: 200 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1, ease: "easeOut" }}
-          className="absolute hidden lg:block top-64 -right-40 w-[350px]"
-        >
-          <Image
-            src={RightPillar}
-            alt="Right Pillar"
-            className="w-full h-auto"
-          />
-        </motion.div>
         <div className="text-white">
           <motion.div
-            className="font-light text-xs md:text-sm text-center"
+            className="font-light text-xs md:text-lg text-center z-2 relative"
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1, ease: "easeOut" }}
@@ -107,8 +63,9 @@ const page = () => {
             initial={{ opacity: 0, scale: 0.9 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 1 }}
+            className="relative z-30"
           >
-            <Image src={Title} alt="Title" className="w-[70%] max-w-[900px] mx-auto" />
+            <Image src={Title} alt="Title" className="w-[70%] max-w-[1200px] mx-auto" />
           </motion.div>
 
           <motion.div
@@ -116,7 +73,7 @@ const page = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1, delay: 0.5 }}
           >
-            <Image src={Board} alt="Board" className="w-[50%] max-w-[400px] z-1 mx-auto" />
+            <Image src={Board} alt="Board" className="w-[50%] max-w-[400px] z-20 mx-auto relative" />
           </motion.div>
         </div>
       </div>
@@ -129,7 +86,7 @@ const page = () => {
           viewport={{ once: true, amount: 0.3 }}
           className="w-full flex justify-center mt-20"
         >
-          <Image src={WhatIs} alt="Logo" className="w-[50%] max-w-lg" />
+          <Image src={WhatIs} alt="Logo" className="w-[50%] max-w-md" />
         </motion.div>
 
         <motion.div
@@ -139,7 +96,7 @@ const page = () => {
           viewport={{ once: true, amount: 0.3 }}
           className="w-full flex justify-center"
         >
-          <Image src={PTDTitle} alt="Logo" className="w-[90%] max-w-6xl mt-6" />
+          <Image src={PTDTitle} alt="Logo" className="w-[90%] max-w-6xl mt-6 md:mt-12" />
         </motion.div>
         <motion.div 
         className="relative w-full mt-16 flex items-center justify-center"
@@ -151,42 +108,9 @@ const page = () => {
           <Image
             src={WhatIsBox}
             alt="Logo"
-            className="inset-0 w-full object-contain z-10"
+            className="inset-0 w-[60%] object-contain z-10"
           />
         </motion.div>
-
-        <motion.div
-          className="w-full flex flex-col gap-2 mt-32"
-        >
-          <motion.div
-            className="w-full flex justify-start"
-            initial={{ opacity: 0, x: -100 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1, ease: "easeOut" }}
-            viewport={{ once: true, amount: 0.3 }}
-          >
-            <Image 
-              src={LeftPattern} 
-              alt="Left Pattern" 
-              className="opacity-30" 
-            />
-          </motion.div>
-
-          <motion.div
-            className="w-full flex justify-end"
-            initial={{ opacity: 0, x: 100 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1, ease: "easeOut" }}
-            viewport={{ once: true, amount: 0.3 }}
-          >
-            <Image 
-              src={RightPattern} 
-              alt="Right Pattern" 
-              className="opacity-30" 
-            />
-          </motion.div>
-        </motion.div>
-
 
         <motion.div 
           className="w-full flex justify-center"
@@ -195,41 +119,8 @@ const page = () => {
           transition={{ duration: 1, delay: 0.6 }}
           viewport={{ once: true, amount: 0.3 }}
           >
-            <Image src={VisiMisi} alt="Logo" className="w-[75%] lg:mt-32" />
+            <Image src={VisiMisi} alt="Logo" className="w-[75%] mt-16 lg:mt-32" />
           </motion.div>
-        <motion.div
-
-          className="w-full flex flex-col gap-2 mt-12"
-        >
-          <motion.div
-            className="w-full flex justify-start"
-            initial={{ opacity: 0, x: -100 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1, ease: "easeOut" }}
-            viewport={{ once: true, amount: 0.3 }}
-          >
-            <Image 
-              src={LeftPattern} 
-              alt="Left Pattern" 
-              className="opacity-30" 
-            />
-          </motion.div>
-
-          <motion.div
-            className="w-full flex justify-end"
-            initial={{ opacity: 0, x: 100 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1, ease: "easeOut" }}
-            viewport={{ once: true, amount: 0.3 }}
-          >
-            <Image 
-              src={RightPattern} 
-              alt="Right Pattern" 
-              className="opacity-30" 
-            />
-          </motion.div>
-        </motion.div>
-
       </div>
       <div className="overflow-hidden relative w-full bg-gradient-to-b from-[#36290A] to-black flex flex-col items-center justify-center py-20 md:py-32">
         <div className="relative z-10 flex flex-col items-center w-full justify-center">
